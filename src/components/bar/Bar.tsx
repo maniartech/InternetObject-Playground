bf80e7665a958349503f74de0bdc7288fcbaec3d
nomaan.maniar <nomaan@maniar.tech>
2023-11-10 19:51:31 +0530

    Fixed layout styling issue and other changes

---
+++ src/components/bar/Bar.tsx
@@ -0,0 +1,17 @@
+import   './Bar.css'
+
+interface BarProps {
+  label: string;
+  bgColor?: string;
+}
+
+
+const Bar = ({ label, bgColor }: BarProps) => {
+  return (
+    <div className='bar' style={{backgroundColor: bgColor}}>
+      <p>{label}</p>
+    </div>
+  )
+}
+
+export default Bar
