0bf9ffdae00b9e46fba553006e2c9f1b4cd37271
Aamir Maniar <aamironline@gmail.com>
2023-11-23 23:14:54 +0530

    Editor dev WIP

--- src/sample-data/simple.ts
+++ src/sample-data/simple.ts
@@ -1,5 +1,4 @@
-const doc = `
-name, age, isActive, joiningDt, address, colors
+const doc = `name, age, isActive, joiningDt, address, colors
 ---
 John Doe, 25, T, d'2022-01-01', {Bond Street, New York, NY}, [red, blue]
 `
