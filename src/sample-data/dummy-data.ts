045383e03957e756bb3aa7562b5f152a4c51b8bb
Aamir Maniar <aamironline@gmail.com>
2023-12-24 18:54:30 +0530

    Update dummy data and schema

--- src/sample-data/dummy-data.ts
+++ src/sample-data/dummy-data.ts
@@ -3,7 +3,13 @@ const doc = `~ recordCount: 22
 ~ page: 3
 ~ prevPage: "/api/v1/people/page/2"
 ~ nextPage: F
-~ @cymk: [cyan, yellow, magenta, black]
+---
+~ John Doe, 25, d'2022-01-01', m, @officeAddr, [red, green, blue]
+~ Jane Done, 20, d'2022-10-10', f, {\\uD83D\\uDCC8 Bond Street, "New York", NY}, [green, purple]
+`
+
+const schema = `# Employee Shema and Variables
+~ @officeAddr: {Santacruze, California, CA}
 ~ $schema: {
     name: string,  # The person name
     age: {int, min:20, max:35},  # The person age!
@@ -11,20 +17,17 @@ const doc = `~ recordCount: 22
     gender?: {string, choices:[m, f, u]},
     address?*: { # Person's address (optional) (Nullable)
       street: string,
-      city: {string, choices:[New York, San Fransisco, Washington]},
+      city: {string, choices:[New York, California, San Fransisco, Washington]},
       state: {string, maxLen:2, choices:[NY, CA, WA]}
     },
     colors?: [string], # Color array inthe form of string array
     isActive?: {bool, F}
   }
----
-~ John Doe, 25, d'2022-01-01', m, {Bond Street, New York, NY}, @cymk, T
-~ Jane Done, 20, d'2022-10-10', f, {\\uD83D\\uDCC8 Bond Street, "New York", NY}, [green, purple]
 `
 
 const exportable = {
   doc,
-  schema: null,
+  schema,
   name: 'full',
 }
 
