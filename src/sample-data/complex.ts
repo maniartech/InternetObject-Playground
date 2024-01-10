7e329ac0e6ec157846849263cb2ab6800245a06d
Aamir Maniar <aamironline@gmail.com>
2024-01-10 13:35:54 +0530

    Refactor sample data structure in complex.ts

--- src/sample-data/complex.ts
+++ src/sample-data/complex.ts
@@ -9,14 +9,24 @@ const schema = `
 
 const doc = `
 City Central Library, '123 Library St, Bookville',
-
-[
-  { The Great Gatsby,F. Scott Fitzgerald,1234567890, true,["Fiction", "Classic"], 1925 },
-  { 1984, George Orwell, 2345678901, false, ["Fiction", "Dystopian"], 1949, { user123, 2024-02-20} }
-],
-[
-  { user123, John Doe,Standard, [{2345678901,"2024-01-20"}] },
-  { user456, Jane Smith, Premium, [] }
+books: [{
+    The Great Gatsby,
+    F. Scott Fitzgerald,
+    1234567890,
+    true,
+    ["Fiction", "Classic"],
+    1925
+}, {
+    "1984, George Orwell",
+    2345678901,
+    false,
+    ["Fiction", "Dystopian"],
+    1949,
+    { user123, 2024-02-20}
+}],
+users: [
+    { user123, John Doe, Standard,  [{2345678901,"2024-01-20"}]  },
+    { user456, Jane Smith, Premium, [] }
 ]
 `.trim()
 
