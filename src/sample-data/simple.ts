6d319721519fccc165bc4f3d6533b1addd88b2b0
Aamir Maniar <aamironline@gmail.com>
2024-01-10 13:14:15 +0530

    Update address field in sample data

--- src/sample-data/simple.ts
+++ src/sample-data/simple.ts
@@ -1,4 +1,4 @@
-const doc = `name, age, isActive, joiningDt, address, colors
+const doc = `name, age, isActive, joiningDt, address: {street, city, state}, colors
 ---
 John Doe, 25, T, d'2022-01-01', {Bond Street, New York, NY}, [red, blue]
 `
