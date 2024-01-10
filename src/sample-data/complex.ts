a47030ad132a14cd74b518523ff22d9177c14e3a
Aamir Maniar <aamironline@gmail.com>
2024-01-10 13:14:06 +0530

    Add complex sample data file

---
+++ src/sample-data/complex.ts
@@ -0,0 +1,28 @@
+const schema = `
+~ $borrower: {userId, dueDate}
+~ $borrowedBooks: {bookIsbn, borrowDate}
+~ $users: {userId, name, membershipType, currentlyBorrowedBooks:[$borrowedBooks]}
+~ $books: {title, author, isbn, availability, categories, published, borrowedBy?: $borrower}
+~ $schema: {name: string, address: string, books: [$books], users: [$users]}
+`.trim()
+
+const doc = `
+City Central Library, '123 Library St, Bookville',
+
+[
+  { The Great Gatsby,F. Scott Fitzgerald,1234567890, true,["Fiction", "Classic"], 1925 },
+  { 1984, George Orwell, 2345678901, false, ["Fiction", "Dystopian"], 1949, { user123, 2024-02-20} }
+],
+[
+  { user123, John Doe,Standard, [{2345678901,"2024-01-20"}] },
+  { user456, Jane Smith, Premium, [] }
+]
+`.trim()
+
+const exportable = {
+  doc,
+  schema,
+  name: 'complex',
+}
+
+export default exportable;
