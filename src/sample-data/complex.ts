e08c1ff7f8efeea846c8633489ca24efd200e057
Aamir Maniar <aamironline@gmail.com>
2024-01-11 10:28:34 +0530

    Update complex.ts schema and sample data

--- src/sample-data/complex.ts
+++ src/sample-data/complex.ts
@@ -1,6 +1,6 @@
 const schema = `
-~ $borrower: {userId, dueDate}
-~ $borrowedBooks: {bookIsbn, borrowDate}
+~ $borrower: {userId, dueDate:date}
+~ $borrowedBooks: {bookIsbn, borrowDate:date}
 ~ $users: {userId, name, membershipType, currentlyBorrowedBooks:[$borrowedBooks]}
 ~ $books: {title, author, isbn, availability, categories, published, borrowedBy?: $borrower}
 ~ $library: {name: string, address: string, books: [$books], users: [$users]}
@@ -8,24 +8,25 @@ const schema = `
 `.trim()
 
 const doc = `
-City Central Library, '123 Library St, Bookville',
+City Central Library, "123 Library St, Bookville",
 books: [{
     The Great Gatsby,
-    F. Scott Fitzgerald,
+    "F. Scott Fitzgerald",
     1234567890,
     true,
-    ["Fiction", "Classic"],
+    [Fiction, Classic],
     1925
 }, {
-    "1984, George Orwell",
+    "1984",
+    George Orwell,
     2345678901,
     false,
-    ["Fiction", "Dystopian"],
+    [Fiction, Dystopian],
     1949,
-    { user123, 2024-02-20}
+    { user123, d"2024-02-20"}
 }],
 users: [
-    { user123, John Doe, Standard,  [{2345678901,"2024-01-20"}]  },
+    { user123, John Doe, Standard,  [{2345678901,d"2024-01-20"}]  },
     { user456, Jane Smith, Premium, [] }
 ]
 `.trim()
