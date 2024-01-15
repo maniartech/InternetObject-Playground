3b5e3adfa36a1db79f2a802030d170944521c834
Aamir Maniar <aamironline@gmail.com>
2024-01-15 16:34:03 +0530

    Update multiple-sections.ts file

--- src/sample-data/multiple-sections.ts
+++ src/sample-data/multiple-sections.ts
@@ -4,16 +4,17 @@ const schema = `
 ~ $users: {userId:string, name:string, membershipType:{type:string, choices:[Standard, Premium]}, currentlyBorrowedBooks:[$borrowedBooks]}
 ~ $books: {title:string, author:string, isbn:number, availability:bool, categories:[string], published:number, borrowedBy?: $borrower}
 ~ $library: {name: string, address: string}
-~ $schema: $library
+# ~ $schema: $library
 `.trim()
 
 const doc = `
+--- $library
 # Bookville Library
 City Central Library, "123 Library St, Bookville"
 
 --- $books
 ~ The Great Gatsby, "F. Scott Fitzgerald", 1234567890, T,[Fiction, Classic], 1925
-~ 1984, George Orwell, 2345678901, F, [Fiction, Dystopian], 1949, { user123, d"2024-02-20"}
+~ "1984", George Orwell, 2345678901, F, [Fiction, Dystopian], 1949, { user123, d"2024-02-20"}
 
 --- $users
 ~ user123, John Doe, Standard,  [{2345678901,d"2024-01-20"}]
