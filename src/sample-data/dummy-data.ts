73d4167865709bdf42f023a4b7766e58e89d8b3b
Aamir Maniar <aamironline@gmail.com>
2024-01-26 00:17:50 +0530

    Update dummy-data.ts with new record count and schema

--- src/sample-data/dummy-data.ts
+++ src/sample-data/dummy-data.ts
@@ -1,29 +1,34 @@
 
-const doc = `~ recordCount: 22
+const doc = `
+~ recordCount: 23
 ~ page: 3
 ~ prevPage: "/api/v1/people/page/2"
-~ nextPage: F
----
-~ John Doe, 25, d'2022-01-01', m, @officeAddr, [red, green, blue]
-~ Jane Done, 20, d'2022-10-10', f, {\\uD83D\\uDCC8 Bond Street, "New York", NY}, [green, purple]
-`
+~ nextPage: T
 
-const schema = `# Employee Schema and Variables
+--- $schema: abc
+~ John Doe, 25, d'2022-01-01', m, @officeAddr,@officeAddr, [red, green, blue]
+~ Jane Done, 20, d'2022-10-10', f, {\uD83D\uDCC8 Bond Street, "New York", NY},N, [green, purple]
+`.trim()
+
+const schema = `
+# Employee Schema and Variables
 ~ @officeAddr: {Santacruze, California, CA}
+~ $address: {
+      street: string,
+      city: {string, choices:[New York, California, San Fransisco, Washington]},
+      state: {string, maxLen:2, choices:[NY, CA, WA]}
+    }
 ~ $schema: {
     name: string,  # The person name
     age: {int, min:20, max:35},  # The person age!
     joiningDt: date,  # The person joining date
     gender?: {string, choices:[m, f, u]},
-    address?*: { # Person's address (optional) (Nullable)
-      street: string,
-      city: {string, choices:[New York, California, San Fransisco, Washington]},
-      state: {string, maxLen:2, choices:[NY, CA, WA]}
-    },
+    currentAddress: $address,
+    permanentAddress?*: $address,
     colors?: [string], # Color array inthe form of string array
     isActive?: {bool, F}
   }
-`
+`.trim()
 
 const exportable = {
   doc,
