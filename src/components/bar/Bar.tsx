af46a9c1bf19060942d7ba85b6603b2d5792e24c
Aamir Maniar <aamironline@gmail.com>
2024-02-14 00:16:31 +0530

    Add isError prop to Bar component

--- src/components/bar/Bar.tsx
+++ src/components/bar/Bar.tsx
@@ -7,19 +7,21 @@ interface BarProps {
   bytes?        : number;
   outputBytes?  : number;
   minified?   : boolean;
+  isError?      : boolean;
 }
 
-const Bar = ({ label, bgColor, children, bytes, outputBytes, minified}: BarProps) => {
+const Bar = ({ label, bgColor, children, bytes, outputBytes, minified, isError}: BarProps) => {
   bytes     = bytes || 0
   const perc = 100 - (outputBytes ? (bytes / outputBytes) * 100 : 0)
   const percText = perc > 0 ? `${perc.toFixed(2)}% Smaller` : `${Math.abs(perc).toFixed(2)}% Larger`
 
+   const showBar = !!bytes && !isError
   return (
     <div className='bar' style={{backgroundColor: bgColor}}>
-      <p>{label} {bytes !== 0 && <span className="count" title={`${bytes} bytes in ${label}`}>{bytes} Bytes</span>}
+      <p>{label} {showBar && <span className="count" title={`${bytes} bytes in ${label}`}>{bytes} Bytes</span>}
       &nbsp;
       </p>
-      {bytes !== 0 && !!outputBytes && <span className="comparision" title={`${bytes} bytes in ${label}`}>{percText} than { minified ? 'minified ' : ''}JSON</span>}
+      {showBar && !!outputBytes && <span className="comparision" title={`${bytes} bytes in ${label}`}>{percText} than { minified ? 'minified ' : ''}JSON</span>}
       { children }
     </div>
   )
