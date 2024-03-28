cde1802e24c96dbc54b6cd53f6dbedb6dd02f204
Aamir Maniar <aamironline@gmail.com>
2024-03-28 16:38:50 +0530

    Update header menu links

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -18,22 +18,22 @@ export default function Header(props: any) {
         <div className="menu-container">
           <ul className="header-menu">
             <li className="header-menuItem">
-              <Link to="/" aria-label="Home">
+              <Link to="https://www.internetobject.org/" aria-label="Home">
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
               </Link>
             </li>
             <li className="header-menuItem">
-              <Link  to="/io-vs-json/" target="_blank" aria-label="IO vs JSON">
+              <Link  to="https://www.internetobject.org/io-vs-json/" target="_blank" aria-label="IO vs JSON">
                 IO vs JSON
               </Link>
             </li>
             <li className="header-menuItem">
-              <Link to="/the-story/" target="_blank" aria-label="The Story">
+              <Link to="https://www.internetobject.org/the-story/" target="_blank" aria-label="The Story">
                 The Story
               </Link>
             </li>
             <li className="header-menuItem">
-              <Link to="/community/" target="_blank" aria-label="Community">
+              <Link to="https://www.internetobject.org/community/" target="_blank" aria-label="Community">
                 Join Community
               </Link>
             </li>
