da17251237059db912eeb286cfad7d78b624462c
Aamir Maniar <aamironline@gmail.com>
2024-03-28 07:16:02 +0530

    Clean live branch

--- src/components/logo/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import                       './Logo.css'
-import logo             from '../../logo.png'
-
-export default function Logo() {
-
-  return (
-    <img src={logo} className="App-logo" alt="logo" />
-  )
-}
--- surge.sh
+++
@@ -1,6 +0,0 @@
-# Ensure you have already installed searge using following command
-# npm install --global surge
-yarn build
-
-cd build
-surge
