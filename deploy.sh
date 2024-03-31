e493cd12723796cb7ca0523e1f311020bc688c35
Aamir Maniar <aamironline@gmail.com>
2024-03-31 12:38:17 +0530

    Refactored deploy script and added functions for setup, building the playground, and pushing changes to live site

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,58 @@
 #!/bin/bash
 
-# Load the .env file
-source .env.deploy
+setup() {
+  # Load the environment variables
+  source ./.env.local
 
-# Check if TARGET_DIR is set
-if [ -z "$TARGET_DIR" ]; then
-    echo "TARGET_DIR is not set in .env file"
-    exit 1
-fi
+  # Check if TARGET_DIR is set
+  if [ -z "$TARGET_DIR" ]; then
+      echo "TARGET_DIR is not set in .env.local file"
+      exit 1
+  fi
 
-# Check if the target directory exists
-if [ ! -d "$TARGET_DIR" ]; then
-    echo "Creating target directory: $TARGET_DIR"
-    mkdir -p "$TARGET_DIR"
-else
-    # Clean the target directory by removing all files
-    echo "Cleaning target directory: $TARGET_DIR"
-    rm -f "$TARGET_DIR"/*
-fi
+  # Check if the target directory exists
+  if [ ! -d "$TARGET_DIR" ]; then
+      echo "Creating target directory: $TARGET_DIR"
+      mkdir -p "$TARGET_DIR"
+  else
+      # Clean the target directory by removing all files
+      echo "Cleaning target directory: $TARGET_DIR"
+      rm -r "$TARGET_DIR"/*
+  fi
+}
 
-# Build the plaground
-yarn build
+build_playground() {
+  # Build the playground
+  yarn build
 
-# # Move files to the target directory
-mv ./build/* "$TARGET_DIR"
+  # # Move files to the target directory
+  cp -r build/* "$TARGET_DIR"
+}
 
-# Change the to the target directory
-cd "$TARGET_DIR"
+push_to_live() {
+  # Change the to the target directory
+  cd "$TARGET_DIR"
 
-# Push the changes to the remote git repository
-git add .
-git commit -m "Deployed on $(date +'%Y-%m-%d %H:%M:%S')"
-git push origin live
+  # Check if there are any changes to deploy
+  if git status --porcelain | grep -q '^[AM]'; then
+    echo
+    echo "Deploying the changes to the live site"
+    echo "--------------------------------------"
+    # Push the changes to the remote git repository
+    git checkout live
+    git add .
+    git commit -m "Deployed on $(date +'%Y-%m-%d %H:%M:%S')"
+    git push origin live
+
+    echo "Deployment completed successfully"
+    echo "Wait for a few seconds for the changes to reflect on"
+    echo "https://play.internetobject.org/"
+
+  else
+    echo "No changes to deploy!"
+  fi
+}
+
+setup
+build_playground
+push_to_live
