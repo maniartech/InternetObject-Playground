eca1b0ac9b7b0c58aceddf288f01619fcecfe478
Aamir Maniar <aamironline@gmail.com>
2024-03-31 23:06:30 +0530

    Refactor sample-data imports

--- src/sample-data/index.ts
+++ src/sample-data/index.ts
@@ -1,20 +1,33 @@
 import dummyData          from "./separate-schema";
-import simple             from "./simple";
 import complex            from "./complex";
 import json               from "./json";
-import simpleCollection   from "./simple-collection";
+
+// Simple
+import simple             from "./simple/simple-object";
+import simpleCollection   from "./simple/simple-collection";
+
+// Schema and Definition
+
+
+
 import multipSections     from "./multiple-sections";
 import employeeRegister   from "./employee-register";
 import recursiveSchema    from "./recursive-schema";
 import recursiveSchemaComplex from "./recursive-schema-comples";
 
-const sampleData:Array<{ doc:string, schema:string | null, name: string, id: string }> = [
+const sampleData:Array<
+{ doc:string, schema:string | null, name: string, id: string } |
+string
+> = [
+  "Simple",
   simple,
+  simpleCollection,
+
+  "Complex",
   complex,
   json,
   recursiveSchema,
   recursiveSchemaComplex,
-  simpleCollection,
   employeeRegister,
   multipSections,
   dummyData,
