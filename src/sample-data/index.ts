947bc4f96a845f3d1b825545f23dbd468174d637
Aamir Maniar <aamironline@gmail.com>
2024-04-01 14:32:36 +0530

    Add simple-object.ts file

--- src/sample-data/index.ts
+++ src/sample-data/index.ts
@@ -8,29 +8,61 @@ import simpleCollection   from "./simple/simple-collection";
 
 // Schema and Definition
 
-
-
 import multipSections     from "./multiple-sections";
 import employeeRegister   from "./employee-register";
 import recursiveSchema    from "./recursive-schema";
 import recursiveSchemaComplex from "./recursive-schema-comples";
 
-const sampleData:Array<
-{ doc:string, schema:string | null, name: string, id: string } |
-string
-> = [
-  "Simple",
-  simple,
-  simpleCollection,
+type SampleMenuItem = { doc:string, schema:string | null, name: string, id: string }
+type SampleMenuGroup = { group: string, items: SampleMenuItem[] }
 
-  "Complex",
-  complex,
-  json,
-  recursiveSchema,
-  recursiveSchemaComplex,
-  employeeRegister,
-  multipSections,
-  dummyData,
+class SampleDataStructure {
+  public groups: SampleMenuGroup[] = []
+  find(id:string):SampleMenuItem | undefined {
+    for (let group of this.groups) {
+      for (let item of group.items) {
+        if (item.id === id) return item
+      }
+    }
+  }
+}
+
+const sampleData:SampleDataStructure = new SampleDataStructure()
+sampleData.groups = [
+  {
+    group: "Simple",
+    items: [
+      simple,
+      simpleCollection
+    ]
+  },
+  {
+    group: "Schema and Definition",
+    items: [
+      employeeRegister,
+      multipSections,
+      recursiveSchema,
+      recursiveSchemaComplex
+    ]
+  },
+  {
+    group: "Complex",
+    items: [
+      complex,
+      json,
+      dummyData
+    ]
+
+  }
+  // "Complex",
+  // complex,
+  // json,
+  // recursiveSchema,
+  // recursiveSchemaComplex,
+  // employeeRegister,
+  // multipSections,
+  // dummyData,
 ]
+
 
 export default sampleData;
