44f9a92c7414b48a820fb30ea0983ac42c7cc588
Aamir Maniar <aamironline@gmail.com>
2024-04-01 19:57:50 +0530

    Update collection and add new objects

---
+++ src/sample-data/simple/simple-object.ts
@@ -0,0 +1,13 @@
+const doc = `name, age, isActive, joiningDt, address: {street, city, state}, colors
+---
+John Doe, 25, T, d'2022-01-01', {Bond Street, New York, NY}, [red, blue]
+`
+
+const exportable = {
+  doc,
+  schema: null,
+  name: 'Single Object',
+  id: 'single-object'
+}
+
+export default exportable;
