e65217ac6a8c601f3b474e9f0c169e0a6bb0a22e
Aamir Maniar <aamironline@gmail.com>
2024-04-05 00:47:35 +0530

    Refactor App component and update sample data handling

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -6,16 +6,17 @@ import Toggle                               from 'react-toggle'
 
 import Header                               from '../../components/header/Header'
 import sampleData                           from '../../sample-data'
+import { SampleOptionItem                 } from '../../sample-data/sample-options';
 import Footer                               from '../footer/Footer'
 import Playground                           from '../playgroud/Playground'
 
 function App (): JSX.Element {
-  const [showSchema, setShowSchema]       = useState(false)
   const [currentDoc, setCurrentDoc]       = useState('')
   const [currentSchema, setCurrentSchema] = useState('')
   const { sampleId }                      = useParams<{ sampleId: string }>()
-  const [ sample, setSample ]             = useState(sampleId || "")
 
+  const [ sample, setSample ]             = useState(sampleData.find(sampleId || ""))
+  const [showSchema, setShowSchema]       = useState(false)
 
   const options = useMemo(() => {
     const options = [
@@ -42,16 +43,16 @@ function App (): JSX.Element {
 
   const handleSampleChange = useCallback((e:any) => {
     const sampleId = e.target.value
+    const sample = { ...sampleData.find(sampleId) } as SampleOptionItem
     const url = `/${sampleId}`
     window.history.pushState({}, '', url)
-    setSample(sampleId)
+    setSample(sample)
   }, [])
 
   useEffect(() => {
-    const sampleInfo = sampleData.find(sample)
-    setCurrentDoc(sampleInfo?.doc || '')
-    if (sampleInfo?.schema) {
-      setCurrentSchema(sampleInfo.schema)
+    setCurrentDoc(sample?.doc || '')
+    if (sample?.schema) {
+      setCurrentSchema(sample.schema)
       setShowSchema(true)
     } else {
       setCurrentSchema('')
@@ -61,7 +62,6 @@ function App (): JSX.Element {
 
   return (
       <div className="app">
-
         <Header>
           <div className='toolbar'>
             <label className='toggle' title="Separate the schema from the data document!">
@@ -76,14 +76,14 @@ function App (): JSX.Element {
               className="highlight"
               title="Select IO sample data"
               onChange={handleSampleChange}
-              value={sample || ''}
+              value={sample?.id || ''}
             >
               {options}
             </select>
           </div>
         </Header>
         <main className='main'>
-          <Playground showSchema={showSchema} setShowSchema={setShowSchema} document={currentDoc} schema={currentSchema} />
+          <Playground showSchema={showSchema} schemaPanelHeight={sample?.schemaPanelHeight} setShowSchema={setShowSchema} document={currentDoc} schema={currentSchema} />
         </main>
         <Footer />
       </div>
