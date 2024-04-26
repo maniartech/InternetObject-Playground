0c7a5eaa14138b6ace9c0952f4e0d9255d05795f
Aamir Maniar <aamironline@gmail.com>
2024-04-26 15:50:33 +0530

    Update sample-data/types/arrays.ts and sample-data/types/objects.ts to improve type definitions

--- src/sample-data/types/arrays.ts
+++ src/sample-data/types/arrays.ts
@@ -8,7 +8,7 @@ array_of_strings: [string],               # array of string
 array_of_strings2: [{string, minLen:2}],  # array of string with minimum length of string required to be 2
 
 # With object notations
-array_of_any_min_len2: {array, minLen:2},  # array of any with minimum length of array required to be 2
+array_of_any_min_len2: {array, minLen:2, of:{string, minLen: 2}},  # array of any with minimum length of array and string to be 2
 array_of_objects:{array, of:{}},
 array_of_objects2: {array, of:{name, age, type}}
 `.trim()
@@ -18,7 +18,7 @@ const doc = `
 [one, 2, 3.0, true, false, null,, d'2021-01-01'],
 [a, b],
 [aa, bbb, cccc],
-[a, b, c],
+[aa, bb, cc],
 [{a: 1, b: 2}, {a: 3, b: 4}],
 [{John Doe, 25, Student}, {Jane Doe, 30, Teacher}]
 `.trim()
--- src/sample-data/types/objects.ts
+++ src/sample-data/types/objects.ts
@@ -1,11 +1,20 @@
 const schema = `
-a:{}, b:object, c: {object, schema:{a, b, c, *}},
-d: {int, min:10}
+a:{}, b:object,                                       # both a and b can accept any object
+c: {object, schema:{a, b, c}}, d: {a, b, c},          # c and d must have a, b, and c members and both of them are not open
+e: {a, b, c, *},                                      # * as the last member in the schema makes object open.
+                                                      # That is it can accept any non-defined members
+f: {a, b?, c?},                                       # here last two members are optional
+g: {a:any, b:{any, optional:T}, c:{any, optional:T}}  # same as f
 `.trim()
 
 const doc = `
-{a, b, c}, {a:1, b:2, c:3}, {a:1, b:2, c:3, d:4},
-{a, b, c}
+{a, b, c},                # a
+{a:1, b:2, c:3},          # b
+{a:1, b:2, c:3},          # c
+{a, b, c},                # d
+{1, 2, 3, d:4, e:5},      # e
+{1},                      # f
+{1}                       # g
 `.trim()
 
 const exportable = {
